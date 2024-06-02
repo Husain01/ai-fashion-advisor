bb58eb240d5043dbfd6b72b4c4dc06ebcccb6d35
Husain Bhagat <hussainbhagat52@gmail.com>
2024-06-02 17:49:08 +0530

    Fix: dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,10 +5,10 @@ import Navbar from "@/app/components/navbar";
 
 export default function Dashboard() {
   const [file, setFile] = useState<File>();
-  const { edgestore } = useEdgeStore();
 
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [selectedImage, setSelectedImage] = useState<File | null>()
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
@@ -49,32 +49,7 @@ export default function Dashboard() {
       <section className="h-screen flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold mb-4">Fashion Advisor</h1>
         <div>
-      <SingleImageDropzone
-        width={200}
-        height={200}
-        value={file}
-        onChange={(file) => {
-          setFile(file);
-        }}
-      />
-      <button
-        onClick={async () => {
-          if (file) {
-            const res = await edgestore.publicFiles.upload({
-              file,
-              onProgressChange: (progress) => {
-                // you can use this to show a progress bar
-                console.log(progress);
-              },
-            });
-            // you can run some server action or api here
-            // to add the necessary data to your database
-            console.log(res);
-          }
-        }}
-      >
-        Upload
-      </button>
+      
     </div>
         {suggestions.length > 0 && (
           <div className="mt-4">
