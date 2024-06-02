2ec02518efb02d41d9e9903f62762189bfb7cf86
Husain Bhagat <hussainbhagat52@gmail.com>
2024-06-02 10:39:12 +0530

    Feat: set up clerk

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -35,12 +35,12 @@ async function main() {
   ])
   console.log(response)
 }
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
